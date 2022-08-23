f3fd9cdbc2c93b9ca93e1ae0226394ae905f0f68
Dianne Tennent <diannetennent@Diannes-MacBook-Pro.local>
2022-08-23 15:56:42 +1200

    Move methods into appropriate places
    - Calculate rounds and errors on the fly instead of
    storing
    
    - Generate hidden word on the fly instead of storing

--- game.rb
+++ game.rb
@@ -4,88 +4,44 @@ require './view'
 class Game
 
   def initialize
-    @state = Model.new(9, 0, 1, [])
+    @state = Model.new(4)
     @view = View.new
-    @visible_word_array = get_word
-    @hidden_word_array = mask_word
-    @game_proper = game_proper
+    run_game
   end
 
-  def game_proper
-    @view.game_intro(@hidden_word_array, @state.rounds, @state.errors, @state.lives)
-    until player_died || word_found
-      @view.status_update(@state.lives, @state.errors, @state.rounds, @hidden_word_array)
+  def run_game
+    @view.game_intro
+
+    until @state.player_died || @state.word_found 
+      hidden = @state.word_array.map{ |x| !@state.guesses.include?(x) ? '*' : x }
+      @view.status_update(@state.lives, @state.number_of_errors, @state.guesses.length() + 1, hidden.join(''))
       guess = @view.get_guess(@state.guesses)
+      store_guess(guess)
       evaluate_guess(guess)
     end
-  end_game
+
+    end_game
   end
 
   def end_game
-    if !@hidden_word_array.include?('*')
-      @view.win_message(@hidden_word_array)
+    if @state.word_found
+      @view.win_message(@state.word_array.join(''))
     else
-      @view.lose_message(@visible_word_array)
+      @view.lose_message(@state.word_array.join(''))
     end
   end
 
   def evaluate_guess(guess)
     # check if letter is in word
-    if @visible_word_array.include?(guess)
-      # replace the letter at that index of the blocked out word
-      @visible_word_array.map.with_index {|x, i|
-      if x == guess
-        @hidden_word_array[i] = guess
-      end
-    }
-    @view.success_message
+    if @state.word_array.include?(guess)
+      @view.success_message
     else
-      increment_errors
       @view.fail_message
     end
-  increment_round
   end
 
-  def player_died
-    if @state.errors >= @state.lives
-      return true
-    else
-      return false
-    end
-  end
-
-  def word_found
-    !@hidden_word_array.include?('*')
-  end
-
-  def increment_round
-    @state.rounds += 1
-  end
-
-  def increment_errors
-    @state.errors += 1
-  end
-
-  def store_guess
-    @state.guesses.append(guess)
-  end
-
-  def get_word
-    # get array of words from file
-    wordsFile = IO.readlines("words.txt")
-    if wordsFile
-      visible_word = wordsFile.sample()
-      # put the visible word in an array
-      visible_word.chomp.split('')
-    else
-      throw "Error"
-    end
-
-  end
-
-  def mask_word
-    # create the hidden word
-    hidden_word_array = @visible_word_array.map { |c| c = '*'}
+  def store_guess(guess)
+    @state.guesses << guess
   end
 
 end
--- view.rb
+++ view.rb
@@ -2,18 +2,17 @@ require 'io/console'
 
 class View
 
-  def game_intro(hidden_word_array, rounds, errors, lives)
+  def game_intro
     # game welcome
     puts "-" * IO.console.winsize[1]
     puts "Welcome to Hangman."
     puts "-" * IO.console.winsize[1]
-
   end
 
   def status_update(lives, errors, rounds, hidden_word_array)
     puts "Lives left: #{lives - errors}"
     puts "Round #{rounds}"
-    puts "Your word is #{hidden_word_array.join('')}"
+    puts "Your word is #{hidden_word_array}" # should give the view the joined array
     puts "-" * IO.console.winsize[1]
     puts "Guess a letter"
     puts "-" * IO.console.winsize[1]
@@ -27,7 +26,6 @@ class View
       if guesses.include?(guess) || guess.length() != 1 || !guess.match?(/[a-z]/)
         puts "Invalid guess, try again."
       else
-        guesses.append(guess)
         valid = true
       end
     end
@@ -45,13 +43,13 @@ class View
   end
 
 
-  def win_message(hidden_word_array)
+  def win_message(answer)
     puts "Congrats - you won."
-    puts "The word is #{hidden_word_array.join('')}"
+    puts "The word is #{answer}"
   end
 
-  def lose_message(visible_word_array)
-    puts "You lost :(\nThe answer was '#{visible_word_array.join('')}'"
+  def lose_message(answer)
+    puts "You lost :(\nThe answer was '#{answer}'"
   end
 
 end
